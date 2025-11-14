a53803172eae603c90f691d4aab06b81d60d44d4
Darshan-netha <129892570+Darshan-netha@users.noreply.github.com>
2025-11-14 22:35:11 +0530

    Add files via upload
    Here You can see the Developers Arena Internship Program Week 1 submission.

---
+++ TASK1.py
@@ -0,0 +1,15 @@
+
+# Internship: Developers Arena - Data Science Domain
+# Week 1 Project
+
+# Asking for user input
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+hobby = input("Enter your favorite hobby: ")
+
+# Displaying a friendly welcome message
+print("\n--- Personal Introduction ---")
+print(f"Hello, {name}! ")
+print(f"It's great to know that you are {age} years old.")
+print(f"Your hobby, {hobby}, sounds really interesting!")
+print("Welcome to the Developers Arena Data Science Internship Program! ")
